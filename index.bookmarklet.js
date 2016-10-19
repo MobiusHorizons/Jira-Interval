5fd7d246dfce638da787748f9ea6e1019370eae1
Paul Martin <codemobius@gmail.com>
2016-10-19 11:14:40 -0400

    cleaned up unnecessary files

--- index.bookmarklet.js
+++ index.bookmarklet.js
@@ -14,13 +14,16 @@
   if (endTime){
     var end = new Date(endTime);
     var interval = end  - startDate;
-    var hours = interval / 1000 / 60 / 60;
+    var hours = Math.floor(interval / 1000 / 60 / 60);
+    var minutes = (interval / 1000 / 60) % 60;
 
-    if (hours < 1){
-      output.value = (hours * 60).toFixed(0) + 'm';
-    } else {
-      output.value = hours.toFixed(2) + 'h';
+    output.value = "";
+
+    if (hours > 0){
+      output.value += hours.toFixed(0) + 'h ';
     }
+    output.value += (minutes).toFixed(0) + 'm';
+
     output.focus();
   }
 }())
